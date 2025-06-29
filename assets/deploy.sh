07fe1a9337e4d00104013c2d76c1062b4d46bf97
Fenhl <fenhl@fenhl.net>
2025-06-29 09:18:49 +0000

    Unix deploy script

---
+++ assets/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+git push
+ssh fenhl.net rustup update stable
+ssh fenhl.net cargo install-update --all --git
+ssh fenhl.net env -C /opt/git/github.com/fenhl/molecule-db/main git pull
+ssh fenhl.net sudo systemctl restart molecule-db
