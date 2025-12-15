2e624bed09ca91d7d6c46d36577129059e6e5d60
Fenhl <fenhl@fenhl.net>
2025-12-15 05:20:46 +0000

    Update deploy script for move to dushanbe

--- assets/deploy.ps1
+++ assets/deploy.ps1
@@ -4,25 +4,13 @@ if (-not $?)
     throw 'Native Failure'
 }
 
-ssh fenhl.net rustup update stable
+ssh mol.fenhl.net env -C git/github.com/fenhl/molecule-db/main git pull
 if (-not $?)
 {
     throw 'Native Failure'
 }
 
-ssh fenhl.net cargo install-update -g molecule-db
-if (-not $?)
-{
-    throw 'Native Failure'
-}
-
-ssh fenhl.net env -C /opt/git/github.com/fenhl/molecule-db/main git pull
-if (-not $?)
-{
-    throw 'Native Failure'
-}
-
-ssh fenhl.net sudo systemctl restart molecule-db
+dev dushanbe update
 if (-not $?)
 {
     throw 'Native Failure'
