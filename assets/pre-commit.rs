5dafd8dbd384f6ffa31814dc3a2434e3bd218759
Fenhl <fenhl@fenhl.net>
2026-03-25 05:29:42 +0000

    Project structure updates

--- assets/pre-commit.rs
+++ assets/pre-commit.rs
@@ -64,6 +64,9 @@ async fn main() -> Result<(), Error> {
         }
     }
 
+    println!("cargo deny");
+    Command::new("cargo").arg("+stable").arg("deny").arg("check").arg("advisories").arg("bans").check("cargo deny").await?;
+
     cfg_select! {
         target_os = "windows" => {
             Command::new("cargo").arg("+stable").arg("check").spawn().at_command("cargo check")?.check("cargo check").await?;
