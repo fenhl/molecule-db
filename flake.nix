505963decf137a019d480bd2a9136e2b7b6feeca
Fenhl <fenhl@fenhl.net>
2026-08-08 06:30:14 +0000

    Simplify Nix flake

--- flake.nix
+++ flake.nix
@@ -1,51 +1,39 @@
 {
     inputs = {
         nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+        flake-utils.url = "github:numtide/flake-utils";
         rust-overlay = {
             url = "github:oxalica/rust-overlay";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
-    outputs = attrs: let
-        supportedSystems = [
-            "aarch64-darwin"
-            "aarch64-linux"
-            "x86_64-darwin"
-            "x86_64-linux"
-        ];
-        forEachSupportedSystem = f: attrs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-            pkgs = import attrs.nixpkgs {
-                inherit system;
-                overlays = [
-                    attrs.rust-overlay.overlays.default # required for cargo-script
-                ];
-            };
-        });
+    outputs = attrs: attrs.flake-utils.lib.eachDefaultSystem (system: let
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+        pkgs = import attrs.nixpkgs {
+            inherit system;
+            overlays = [
+                attrs.rust-overlay.overlays.default # required for cargo-script
+            ];
+        };
     in {
-        devShells = forEachSupportedSystem ({ pkgs, ... }: pkgs.mkShell {
-            pre-commit = pkgs.mkShell {
-                packages = with pkgs; [
-                    rust-bin.nightly.latest.default # nightly cargo, required to run the pre-commit script
-                    cargo-deny
-                ];
+        devShells.pre-commit = pkgs.mkShell {
+            packages = with pkgs; [
+                rust-bin.nightly.latest.default # nightly cargo, required to run the pre-commit script
+                cargo-deny
+            ];
+        };
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+            pname = "molecule-db";
+            version = manifest.version;
+            buildFeatures = [
+                "night"
+                "nixos"
+            ];
+            cargoLock = {
+                allowBuiltinFetchGit = true; # allows omitting cargoLock.outputHashes
+                lockFile = ./Cargo.lock;
             };
-        });
-        packages = forEachSupportedSystem ({ pkgs, ... }: let
-            manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        in {
-            default = pkgs.rustPlatform.buildRustPackage {
-                pname = "molecule-db";
-                version = manifest.version;
-                buildFeatures = [
-                    "night"
-                    "nixos"
-                ];
-                cargoLock = {
-                    allowBuiltinFetchGit = true; # allows omitting cargoLock.outputHashes
-                    lockFile = ./Cargo.lock;
-                };
-                src = ./.;
-            };
-        });
-    };
+            src = ./.;
+        };
+    });
 }
