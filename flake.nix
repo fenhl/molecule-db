af30f3839a6acf200cc856b61d248281973ea89e
Fenhl <fenhl@fenhl.net>
2026-08-16 14:42:57 +0000

    Replace flake-utils with my own helper

--- flake.nix
+++ flake.nix
@@ -1,30 +1,26 @@
 {
     inputs = {
-        nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
-        flake-utils.url = "github:numtide/flake-utils";
+        flake.url = "github:fenhl/flake";
         rust-overlay = {
             url = "github:oxalica/rust-overlay";
-            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.nixpkgs.follows = "flake/nixpkgs";
         };
     };
-    outputs = attrs: attrs.flake-utils.lib.eachDefaultSystem (system: let
-        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        pkgs = import attrs.nixpkgs {
-            inherit system;
-            overlays = [
-                attrs.rust-overlay.overlays.default # required for cargo-script
-            ];
-        };
-    in {
-        devShells.pre-commit = pkgs.mkShell {
+    outputs = attrs: attrs.flake.lib {
+        overlays = [
+            attrs.rust-overlay.overlays.default # required for cargo-script
+        ];
+        devShells.pre-commit = { pkgs, ... }: {
             packages = with pkgs; [
                 rust-bin.nightly.latest.default # nightly cargo, required to run the pre-commit script
                 cargo-deny
             ];
         };
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+        packages.default = { pkgs, ... }: let
+            manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+        in pkgs.rustPlatform.buildRustPackage {
+            inherit (manifest) version;
             pname = "molecule-db";
-            version = manifest.version;
             buildFeatures = [
                 "night"
                 "nixos"
@@ -35,5 +31,5 @@
             };
             src = ./.;
         };
-    });
+    };
 }
